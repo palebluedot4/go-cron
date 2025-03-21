ae0f7a09e12f818eed4d60bd69358ac619d6e1f6
Bruce <162610581+palebluedot4@users.noreply.github.com>
2025-03-21 22:43:26 +0800

    feat: add timeutil package with date and time formatting and Taipei timezone support

---
+++ pkg/utils/timeutil/timeutil.go
@@ -0,0 +1,21 @@
+package timeutil
+
+import (
+	"time"
+)
+
+const (
+	DateFormat     = "2006-01-02"
+	TimeFormat     = "15:04:05"
+	DateTimeFormat = "2006-01-02 15:04:05"
+)
+
+var TaipeiLocation *time.Location
+
+func init() {
+	var err error
+	TaipeiLocation, err = time.LoadLocation("Asia/Taipei")
+	if err != nil {
+		TaipeiLocation = time.FixedZone("Asia/Taipei", 8*60*60)
+	}
+}
